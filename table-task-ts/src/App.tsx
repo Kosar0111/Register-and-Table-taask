11306f3b2a753fa9aef9429d4f89e458a0f3bd81
Konstantin Loktiev <81988686+Kosar0111@users.noreply.github.com>
2023-10-23 20:43:42 +0300

    start

---
+++ table-task-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="log-form"></header>
+      <div className="App-content">
+        <p>table task</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
